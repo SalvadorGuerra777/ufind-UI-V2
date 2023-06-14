25c393ccb844fc3264e4ab919749456f5db3c0e0
Pyload901 <00060021@uca.edu.sv>
2023-06-14 11:15:24 -0600

    add viewmodel de main activity para validar un token existente

--- app/src/main/java/org/ufind/MainActivity.kt
+++ app/src/main/java/org/ufind/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,27 +23,32 @@ import org.ufind.data.OptionsRoutes
 import org.ufind.data.datastore.DataStoreManager
 import org.ufind.data.model.UserModel
 import org.ufind.navigation.NavigationComponent
+import org.ufind.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel: MainViewModel = viewModel<MainViewModel>()
             val dataStoreManager = DataStoreManager(LocalContext.current)
-
+            val startDestination = viewModel.startDestination.collectAsStateWithLifecycle()
             val navController = rememberNavController()
-            LaunchedEffect(this@MainActivity) {
+            LaunchedEffect(dataStoreManager.getUserData()) {
                 lifecycleScope.launch {
                     val user = dataStoreManager.getUserData()
                     user.collect{
-
+                        Log.d("APP_TAG", it.toString())
+                        if(it.token != "")
+                            viewModel.updateStartDestination(OptionsRoutes.UserInterface.route)
+                        else
+                            viewModel.updateStartDestination(OptionsRoutes.LogInOrSignUpOptions.route)
                     }
                 }
             }
-            val user by dataStoreManager.getUserData().collectAsStateWithLifecycle(initialValue = UserModel(0,"","","",""), lifecycle = LocalLifecycleOwner.current.lifecycle)
 
 //            val startDestionation = if (user.token == "") OptionsRoutes.LogInOrSignUpOptions.route else OptionsRoutes.UserInterface.route
-            NavigationComponent(navHostController = navController, startDestination = OptionsRoutes.LogInOrSignUpOptions.route)
-            Log.d("APP_TAG", user.toString())
+            NavigationComponent(navHostController = navController, startDestination = startDestination.value)
+//            Log.d("APP_TAG", user.toString())
 //            NavigationView()
         }
     }
--- app/src/main/java/org/ufind/viewmodel/MainViewModel.kt
+++ app/src/main/java/org/ufind/viewmodel/MainViewModel.kt
@@ -2,9 +2,16 @@ package org.ufind.viewmodel
 
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.ufind.data.OptionsRoutes
+import org.ufind.data.datastore.DataStoreManager
 
 class MainViewModel(): ViewModel() {
-    val isLogged = mutableStateOf(false)
+    var startDestination = MutableStateFlow(OptionsRoutes.LogInOrSignUpOptions.route)
 
+    fun updateStartDestination(value: String) {
+        startDestination.value = value
+    }
 }
