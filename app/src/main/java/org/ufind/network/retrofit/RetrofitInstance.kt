198c9f6d34e9ba1d2c1455512e6bd55424983e78
Pyload901 <00060021@uca.edu.sv>
2023-06-21 13:07:24 -0600

    fix ip de api

--- app/src/main/java/org/ufind/network/retrofit/RetrofitInstance.kt
+++ app/src/main/java/org/ufind/network/retrofit/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import org.ufind.network.service.UserService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "http://34.174.36.97:3000/api/"
+const val BASE_URL = "http://34.174.97.231:3000/api/"
 object RetrofitInstance {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
