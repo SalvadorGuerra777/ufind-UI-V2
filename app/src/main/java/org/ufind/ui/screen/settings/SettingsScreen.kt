513b05236fd2c2275f2fcdfaa7e8b8121a52e153
Pyload901 <00060021@uca.edu.sv>
2023-06-23 16:47:10 -0600

    merge feature/logout -> develop

--- app/src/main/java/org/ufind/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/org/ufind/ui/screen/settings/SettingsScreen.kt
@@ -14,8 +14,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowRight
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
